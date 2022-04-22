0870e88bae5696f9ad9601ca493f147439c34695
AamerShikari <shikariaamer@gmail.com>
2022-04-22 12:43:19 -0400

    Homepage and About page routes and views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return HttpResponse('<h1>this is the about page, kacchow!<h1>')
