d7bbdce33f44b91537abb2cba5984738cdeb1ea3
AamerShikari <shikariaamer@gmail.com>
2022-04-22 13:08:20 -0400

    Styling of about page, adding of templates and base.html template

--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,4 @@ def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
-    return HttpResponse('<h1>this is the about page, kacchow!<h1>')
+    return render(request, 'about.html')
